cb1631ddae583d1cce30d09bdd1cd3eecbd55ef8
alen888 <46855349+alen888@users.noreply.github.com>
2021-12-11 19:44:32 +0800

    Add files via upload

--- tmp.py
+++ tmp.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 url = "https://www.google.com"
 #driver = webdriver.Chrome()
-driver = webdriver.Chrome("/var/lib/jenkins/workspace/Freestyle_0001/chromedriver")
+driver = webdriver.Chrome("/usr/lib/python3/dist-packages/chromedriver")
 driver.get(url)
 print(driver.page_source)
 driver.close()
