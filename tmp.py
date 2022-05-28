09def2b8f7257328f0e2e73fd04bf27767e56909
alen888 <46855349+alen888@users.noreply.github.com>
2022-05-28 15:37:55 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 
 url = "https://www.google.com"
 #driver = webdriver.Chrome()
-driver = webdriver.Chrome("/usr/lib/python3/dist-packages/chromedriver")
+#driver = webdriver.Chrome("/usr/lib/python3/dist-packages/chromedriver")
+driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 driver.get(url)
 print(driver.page_source)
 driver.close()
